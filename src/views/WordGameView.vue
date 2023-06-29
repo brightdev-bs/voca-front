a8a5bc51365e726c61df072eb57f701b879e5e39
Byeongsu KIM <brightdev.bs@gmail.com>
2023-06-29 13:32:23 +0900

    단어 게임 재채점시 맞았음에도 빨간 박스 표시되는 문제 해결

--- src/views/WordGameView.vue
+++ src/views/WordGameView.vue
@@ -78,37 +78,47 @@ export default {
       let count = 0;
       let wrongWords = new Array();
       let incorrect = [];
-      for(const word of this.words) {
+      for(let i = 0; i < this.words.length; i++) {
+        const word = this.words[i];
+        console.log("words[i] : ", word);
+
         let correctFlag = false;
+
         // 정규식을 이용해 () [], 띄어쓰기 무시한다.
         // https://regexr.com/
         if(word.definition && word.text) {
           const def = word.definition.replace(/\s|^\[.*\]|\(.*\)/g, '');
           const txt = word.text.replace(/\s|^\[.*\]|\(.*\)/g, '');
 
-          if(def == txt) {
+          if (def === txt) {
             correctFlag = true;
+            this.words[i].isWrong = false;
             count++;
             continue;
           }
 
           const defArr = new Set(def.split(","));
           const txtArr = txt.split(",");
-          for(const text of txtArr) {
-            if(defArr.has(text)) {
+
+          for (const text of txtArr) {
+            if (defArr.has(text)) {
               correctFlag = true;
+              this.words[i].isWrong = false;
               count++;
             }
           }
         }
 
-        if(correctFlag == false) {
+        if(correctFlag === false) {
           wrongWords.push(word.word);
-          incorrect.push(word.num);
+          incorrect.push(i);
         }
       }
 
+      // 로직이
       for(const id of incorrect) {
+        console.log(id);
+        console.log(this.words[id]);
         this.words[id].isWrong = true;
       }
 
