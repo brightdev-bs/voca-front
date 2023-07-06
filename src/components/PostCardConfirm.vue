0038917ba12ab2f66c541c23cfa75b838e5c3397
Byeongsu KIM <brightdev.bs@gmail.com>
2023-07-06 15:07:57 +0900

    커뮤니티 생성 -> 맴버 관리 페이지

---
+++ src/components/PostCardConfirm.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card
+      class="mx-auto"
+      width="350"
+  >
+
+    <v-card-text>
+      <h3 class="me-2">{{ name }}</h3>
+      <p> {{ introduction }} </p>
+    </v-card-text>
+
+    <v-card-actions class="float-end me-2">
+      <v-btn size="small" variant="tonal">
+        reject
+      </v-btn>
+      <v-btn size="small" variant="tonal">
+        accept
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+</template>
+
+<script>
+
+export default {
+  props: {
+    name: String,
+    introduction: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ManageMemberView.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>| Requested |</h2>
+  <PostCardConfirm
+      v-for="request in this.memberRequest"
+      :key="request.id"
+      :name="request.name"
+      :introduction="request.introduction"
+  />
+</template>
+
+<script>
+
+import PostCardConfirm from "@/components/PostCardConfirm.vue";
+
+export default {
+  components: {PostCardConfirm},
+  data() {
+    return {
+      memberRequest: {},
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
