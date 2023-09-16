f10c23db62aeed290e69f59793bf94cb924f344d
Byeongsu KIM <brightdev.bs@gmail.com>
2023-09-16 19:45:33 +0900

    비밀번호 찾기 페이지 수정

--- src/views/account/FindPasswordView.vue
+++ src/views/account/FindPasswordView.vue
@@ -1,19 +1,24 @@
 <template>
-  <h2 class="ma-5">Find password</h2>
-  <v-text-field
-      v-model="state.email"
-      :error-messages="v$.email.$errors.map(e => e.$message)"
-      label="E-mail"
-      required
-      @input="v$.email.$touch"
-      @blur="v$.email.$touch"
-  ></v-text-field>
-  <v-btn
-      class="me-4 float-end"
-      @click="sendEmail"
-  >
-    send
-  </v-btn>
+
+  <div class="container">
+    <div class="form login">
+      <div class="form-content">
+        <header>Find Password</header>
+
+        <div class="field input-field">
+          <input type="email" placeholder="Email" @blur="v$.email.$touch" v-model="state.email" class="input">
+          <div v-if="v$.email.$error" class="error-message">The Format isn't correct</div>
+        </div>
+
+        <v-btn
+            class="float-end mt-2"
+            @click="sendEmail"
+        >
+          send
+        </v-btn>
+      </div>
+    </div>
+  </div>
 
   <LoadingAlert :loading="loading"/>
 </template>
@@ -80,5 +85,105 @@ export default {
 </script>
 
 <style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+.container{
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: white;
+  column-gap: 30px;
+}
+.form{
+  position: absolute;
+  max-width: 430px;
+  width: 100%;
+  padding: 30px;
+  border-radius: 6px;
+  background: #FFF;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+}
 
+header{
+  font-size: 28px;
+  font-weight: 600;
+  color: #232836;
+  text-align: center;
+}
+form{
+  margin-top: 30px;
+}
+.form .field{
+  position: relative;
+  height: 50px;
+  width: 100%;
+  margin-top: 20px;
+  border-radius: 6px;
+}
+.field input,
+.field button{
+  height: 100%;
+  width: 100%;
+  border: none;
+  font-size: 16px;
+  font-weight: 400;
+  border-radius: 6px;
+}
+.field input{
+  outline: none;
+  padding: 0 15px;
+  border: 1px solid#CACACA;
+}
+.field input:focus{
+  border-bottom-width: 2px;
+}
+
+.field button{
+  color: #fff;
+  background-color: #0171d3;
+  transition: all 0.3s ease;
+  cursor: pointer;
+}
+.field button:hover{
+  background-color: #016dcb;
+}
+
+.form-link span,
+.form-link a{
+  font-size: 14px;
+  font-weight: 400;
+  color: #232836;
+}
+.form a{
+  color: #0171d3;
+  text-decoration: none;
+}
+.form-content a:hover{
+  text-decoration: underline;
+}
+
+.media-options a{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+a.google span{
+  font-weight: 500;
+  opacity: 0.6;
+  color: #232836;
+}
+
+@media screen and (max-width: 400px) {
+  .form{
+    padding: 20px 10px;
+  }
+
+}
 </style>
