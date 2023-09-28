f77dd373590910939696580f4a37399ce85e3871
Byeongsu KIM <brightdev.bs@gmail.com>
2023-09-29 00:15:04 +0900

    단어 삭제

--- src/components/voca/VocaTable.vue
+++ src/components/voca/VocaTable.vue
@@ -15,6 +15,7 @@
     </thead>
     <tbody>
     <tr
+        @contextmenu.prevent="showContextMenu(word.id, $event)"
         v-for="(word, index) in words"
         :key="word.id"
     >
@@ -32,19 +33,33 @@
     </tr>
     </tbody>
   </v-table>
+
+  <div v-if="contextMenu.show" class="context-menu" :style="{ top: contextMenu.y + 'px', left: contextMenu.x + 'px' }">
+    <ul>
+      <li @click="editWord(contextMenu.index)">Edit</li>
+      <li @click="deleteWord(contextMenu.index)">Delete</li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
 import axios from "axios";
+import {useAxios} from "@/composables/useAxios";
 
 export default {
   props: {
     words: Array,
     isHidingDefinition: Boolean,
   },
-  data () {
+  data() {
     return {
-
+      contextMenu: {
+        show: false,
+        index: null,
+        x: 0,
+        y: 0,
+      },
     }
   },
   methods: {
@@ -68,7 +83,42 @@ export default {
     },
     editWord(index) {
       this.$router.push({name:'AddWord', params: { word: JSON.stringify(this.words[index])}});
-    }
+    },
+    deleteWord(index) {
+      const { dateExecute } = useAxios(
+            '/v1/words/' + index,
+          {
+            method: 'delete',
+            headers: {
+              "Content-Type": 'application/json',
+              Authorization: localStorage.getItem("token"),
+            },
+          },
+          {
+            immediate: false,
+            onSuccess: res => {
+              console.log(res);
+            },
+            onError: err => {
+              alert(err.data.data);
+            }
+          },
+      );
+      dateExecute();
+    },
+    showContextMenu(index, event) {
+      event.preventDefault();
+      this.contextMenu.show = true;
+      this.contextMenu.index = index;
+      this.contextMenu.x = event.clientX;
+      this.contextMenu.y = event.clientY;
+
+      document.addEventListener('click', this.closeContextMenu);
+    },
+    closeContextMenu() {
+      this.contextMenu.show = false;
+      document.removeEventListener('click', this.closeContextMenu);
+    },
   }
 }
 </script>
@@ -80,5 +130,27 @@ export default {
 }
 .v-col v-col-auto {
   padding: 0;
+}
+.context-menu {
+  position: absolute;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.1);
+  z-index: 9999;
+}
+
+.context-menu ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.context-menu ul li {
+  padding: 8px 12px;
+  cursor: pointer;
+}
+
+.context-menu ul li:hover {
+  background-color: #f0f0f0;
 }
 </style>
